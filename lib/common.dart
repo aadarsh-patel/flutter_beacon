a7d40163122c3238cf4abafc37bfe6ab0226cdbb
aadarsh-patel <patelaadarsh0@gmail.com>
2020-03-12 16:40:59 +0530

    Changed the theme from blue to pink

---
+++ lib/common.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+LinearGradient myGradient = LinearGradient(
+  colors: [
+    Colors.pink[400],
+    Colors.pink[100],
+    Colors.pink[400],
+  ],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+);
+
+
+ThemeData myTheme = ThemeData(
+        fontFamily: 'Arvo',
+        primarySwatch: Colors.pink,
+      );
+
+Color myBoxColor = Colors.pink[500];
+Color myTextColor = Colors.white;
+
